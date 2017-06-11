c2c68b180c13362b00bb660e1806981d2c243d08
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2017-06-10 23:05:02 -0700

    Link : https://www.hackerrank.com/challenges/sherlock-and-the-beast

--- Python3/Algorithms/Greedy/Sherlock and the Beast.py
+++ Python3/Algorithms/Greedy/Sherlock and the Beast.py
@@ -1,42 +1,38 @@
-from math import floor
 from collections import Counter
+from math import ceil
 
 number_of_testcases = int(input())
 
 for _ in range(number_of_testcases):
     digits = int(input())
     result = -1
+    found = False
 
+    # base case: digits = 5 (only case that can't be handled by the algorithm
+    if digits == 5:
+        result = "33333"
+        found = True
 
-    # first we try numbers mod 3, this will eliminate the most possibilities
-    #   because it is the highest possible value
-    number_found = False
+    # first check for divisibility mod 3, since that eliminates the most possibilities
     if digits % 3 == 0:
-        result = "5" * (3 * (digits // 3))
-        number_found = True
+        result = "5" * digits
+        found = True
 
-    # make sure we haven't already found the result (if it was divisible by 3)
-    if not number_found:
+    if not found:
+        # for numbers larger than the sum of the two (other than 5, which has already been checked)
+        if digits >= 5 + 3:
 
-        # base base: check the four values that cannot cannot be factored by some combination of 3 and 5
-        #   if digits is one of these values, skip the next step and print result as -1
-        if (digits != 1) and (digits != 2) and (digits != 4) and (digits != 7):
+            # take a multiple of 3 that is minimally larger than digits
+            total_5s = ceil(digits / 3) * 3
 
-            # we take the dividend of digits divided by 3, which will round down
-            #   then we multiply by three to get as close as possible to digits while being less than digits
-            number_of_fives = digits // 3
-            number_of_fives *= 3
+            total_3s = 0
+            # reduce groups of "555" + "555" (length 6), and replace them with "33333" (length 5)
+            #   until the sum of the two is equal to digits
+            while (total_3s + total_5s > digits):
+                total_5s -= 6
+                total_3s += 5
 
-            # add clusters of three 5's until there are more 5's than digits
-            while (number_of_fives < digits):
-                number_of_fives += 3
-
-            # we take away six "5"s and add five "3"s until the total of the two is equal to digits
-            number_of_threes = 0
-            while (number_of_fives + number_of_threes > digits):
-                number_of_fives -= 6
-                number_of_threes += 5
-
-            result = ("5" * number_of_fives) + ("3" * number_of_threes)
+            # build the result
+            result = ("5" * total_5s) + ("3" * total_3s)
 
     print(result)
