d1c3557c03009f31b62408089cc160a92c94a4e5
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2017-06-16 16:40:05 -0700

    Update Remove duplicate values.py

--- Python3/Data Structures/Linked List/Remove duplicate values.py
+++ Python3/Data Structures/Linked List/Remove duplicate values.py
@@ -1,21 +1,9 @@
 def RemoveDuplicates(head):
-    curr_node = head
-    while curr_node:
-        if not curr_node.next:
-            break
-        if curr_node.data != curr_node.next.data:
-            curr_node = curr_node.next
-        else:
-            next_node = curr_node.next
-            while next_node and next_node.next:
-                if curr_node.data == next_node.data:
-                    next_node = next_node.next
-                else:
-                    break
-            if curr_node.data == next_node.data:
-                curr_node.next = None
-                break
+    if head:
+        node = head
+        while node.next:
+            if node.data == node.next.data:
+                node.next = node.next.next
             else:
-                curr_node.next = next_node
-                curr_node = curr_node.next
+                node = node.next
     return head
