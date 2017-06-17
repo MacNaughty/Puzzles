b200f8fbbfdd11537cee7796de34d3874429c931
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2017-06-17 14:15:40 -0700

    Update Find Merge Point of Two Lists.py

--- Python3/Data Structures/Linked List/Find Merge Point of Two Lists.py
+++ Python3/Data Structures/Linked List/Find Merge Point of Two Lists.py
@@ -8,50 +8,64 @@
    def __init__(self, data=None, next_node=None):
        self.data = data
        self.next = next_node
-       
-we assume the two lists have a none null head and a tail (they are not cyclical)
+
+ 
 """
+
+def has_cycle(head):
+    curr_node_slow = head
+    curr_node_fast = head.next
+    while curr_node_fast and curr_node_slow:
+        if id(curr_node_fast) == id(curr_node_slow):
+            return curr_node_fast
+        elif curr_node_fast.next:
+            curr_node_slow = curr_node_slow.next
+            curr_node_fast = curr_node_fast.next.next
+        else:
+            return None
+
 
 def FindMergeNode(headA, headB):
     length_A = 0
     length_B = 0
 
-    curr_node_A = headA
-    curr_node_B = headB
+    if not has_cycle(headA) and not has_cycle(headB):
 
-    while curr_node_A:
-        length_A += 1
-        curr_node_A = curr_node_A.next
+        curr_node_A = headA
+        curr_node_B = headB
 
-    while curr_node_B:
-        length_B += 1
-        curr_node_B = curr_node_B.next
+        while curr_node_A:
+            length_A += 1
+            curr_node_A = curr_node_A.next
 
-    curr_node_A = headA
-    curr_node_B = headB
+        while curr_node_B:
+            length_B += 1
+            curr_node_B = curr_node_B.next
 
+        curr_node_A = headA
+        curr_node_B = headB
 
-    while length_A > length_B:
-        length_A -= 1
-        curr_node_A = curr_node_A.next
+        while length_A > length_B:
+            length_A -= 1
+            curr_node_A = curr_node_A.next
 
-    while length_A < length_B:
-        length_B -= 1
-        curr_node_B = curr_node_B.next
+        while length_A < length_B:
+            length_B -= 1
+            curr_node_B = curr_node_B.next
 
-    result = None
-    """
-    if these were actually two lists that merged, you would check their id's as follows:
-    while curr_node_A and curr_node_B:
-        if id(curr_node_A) == id(curr_node_B):
-            result = curr_node_A.data
-
-        curr_node_A = curr_node_A.next
-        curr_node_B = curr_node_B.next
+        result = None
+        """
+        if these were actually two lists that merged, you would check their id's as follows:
+        while curr_node_A and curr_node_B:
+            if id(curr_node_A) == id(curr_node_B):
+                result = curr_node_A.data
     
-    but since they're actually two different lists with similar values, we use the following code to pass the tests
-    """
-    while curr_node_A and curr_node_B:
+            curr_node_A = curr_node_A.next
+            curr_node_B = curr_node_B.next
+    
+        but since they're actually two different lists with similar values, we use the following code to pass the tests
+        """
+        while curr_node_A and curr_node_B:
             if curr_node_A.data == curr_node_B.data:
                 if result == None:
                     result = curr_node_A.data
@@ -61,4 +75,25 @@ def FindMergeNode(headA, headB):
             curr_node_A = curr_node_A.next
             curr_node_B = curr_node_B.next
 
-    return result
+        return result
+
+    elif has_cycle(headA) and has_cycle(headB):
+        break_node_A = has_cycle(headA)
+        break_node_B = has_cycle(headA)
+
+        if id(break_node_A) == id(break_node_B):
+            curr_node_A = headA
+            while not id(curr_node_A) == id(break_node_A):
+                curr_node_B = headB
+                while not id(curr_node_B) == id(break_node_A): # we've established that break_node_A and break_node_B are the same
+                    if curr_node_B is curr_node_A:
+                        return curr_node_A.data
+                    else:
+                        curr_node_B = curr_node_B.next
+                        
+                curr_node_A = curr_node_A.next
+            return break_node_A.data
+
+
+    else:
+        return False
