04d17947d0cc0ba745c1c91bd9c0ed459d255257
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-03-04 17:37:50 -0800

    Update StringBuilder.kt

--- Kotlin/Greedy/sherlock-and-the-beast/StringBuilder.kt
+++ Kotlin/Greedy/sherlock-and-the-beast/StringBuilder.kt
@@ -1,3 +1,18 @@
+/*
+This is my first solution for the problem. While tidying up I realized there was a better solution using a CharArray (in same parent directory as this file).
+Problem: 
+
+A Decent Number has the following properties:
+    1. Its digits can only be 3's and/or 5's.
+    2. The number of 3's it contains is divisible by 5.
+    3. The number of 5's it contains is divisible by 3.
+    4. It is the largest such number for its length.
+Given an integer n, where 1 <= n <= 100000
+Find the decent number for n
+https://www.hackerrank.com/challenges/sherlock-and-the-beast/problem?isFullScreen=false&utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+*/
+
+
 fun decentNumber(n: Int): Unit {
     // Having seen a similar problem in school, 
     // For any integer m > 7, there exists integers x & y such that 5*x + 3*y = m
