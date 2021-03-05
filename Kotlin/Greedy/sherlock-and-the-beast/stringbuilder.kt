05cd41498ba8911fe59a81e3766d84ded2e2569a
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-03-04 16:52:43 -0800

    Hackerrank Sherlock and the Beast
    A Decent Number has the following properties:
    
    1.    Its digits can only be 3's and/or 5's.
    2.    The number of 3's it contains is divisible by 5.
    3.    The number of 5's it contains is divisible by 3.
    4.    It is the largest such number for its length.
    
    
    Given an integer n, where 1 <= n <= 100,000 print the decent number with n digits. If no such number is possible, return -1
    
    https://www.hackerrank.com/challenges/sherlock-and-the-beast/problem?isFullScreen=false&utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign

---
+++ Kotlin/Greedy/sherlock-and-the-beast/stringbuilder.kt
@@ -0,0 +1,39 @@
+fun decentNumber(n: Int): Unit {
+    // Having seen a similar problem in school, 
+    // For any integer m > 7, there exists integers x & y such that 5*x + 3*y = m
+    // 5 and 3 can be treated as special cases
+    when (n) {
+        1 , 2, 4, 7 -> println(-1)
+        return
+    }
+    
+    // The largest such number will contain as many leading 5's as possible
+    val remainderThree = n.rem(3)
+    val dividend = n / 3
+    
+    val stringBuilder = StringBuilder()
+    if (remainderThree == 0) {
+        // We can simply use all 5's, added in triplets
+        for (i in 1..dividend) {
+            stringBuilder.append("555")
+        }
+        println(stringBuilder.toString())
+        return
+    } else if (remainderThree == 1) {
+      
+        for (i in 1..(dividend - 3)) {
+            stringBuilder.append("555")
+        }
+        stringBuilder.append("3333333333")
+        println(stringBuilder.toString())
+        return
+    } else {
+        // (remainderThree == 2)
+        for (i in 1..(dividend - 1)) {
+            stringBuilder.append("555")
+        }
+        stringBuilder.append("33333")
+        println(stringBuilder.toString())
+        return
+    }
+}
