22b100b1540f44bae7a07d75918181be5743b2ca
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-04-19 11:50:39 -0700

    Create RemoveDuplicatesFromSortedArray.go

---
+++ Go/LeetCode/RemoveDuplicatesFromSortedArray.go
@@ -0,0 +1,22 @@
+func removeDuplicates(nums []int) int {
+	numsLength := len(nums)
+
+	if numsLength == 0 {
+		return 0
+	}
+
+	newIndex := 0
+	oldIndex := 1
+
+	for oldIndex < numsLength {
+		if nums[newIndex] == nums[oldIndex] {
+			oldIndex++
+		} else {
+			nums[newIndex+1] = nums[oldIndex]
+			newIndex++
+			oldIndex++
+		}
+	}
+
+	return newIndex + 1
+}
