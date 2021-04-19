56e1a99617d42c28ad9b016e5adcb2e050a57afb
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-04-19 11:33:31 -0700

    Create LongestCommonPrefix.go

---
+++ Go/LeetCode/LongestCommonPrefix.go
@@ -0,0 +1,25 @@
+func longestCommonPrefix(strs []string) string {
+	strsLength := len(strs)
+
+	if strsLength == 0 {
+		return ""
+	}
+
+	var result strings.Builder
+
+	j := 0
+
+	for len(strs[0]) > j {
+		byteToCheck := strs[0][j]
+
+		for i := 1; i < strsLength; i++ {
+			if len(strs[i]) <= j || byteToCheck != strs[i][j] {
+				return result.String()
+			}
+		}
+		result.WriteByte(byteToCheck)
+		j++
+	}
+
+	return result.String()
+}
