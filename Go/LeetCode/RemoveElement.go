d83e643b7483246957f51ae1111e5bc289d55826
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-04-20 11:59:25 -0700

    Create RemoveElement.go

---
+++ Go/LeetCode/RemoveElement.go
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/remove-element/submissions/
+
+func removeElement(nums []int, val int) int {
+	i := 0
+
+	for _, num := range nums {
+		if num != val {
+			nums[i] = num
+			i++
+		}
+	}
+	return i
+}
