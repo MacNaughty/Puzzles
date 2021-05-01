12efe213bcd418964d658ba2e5b56800338046b0
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-05-01 12:04:31 -0700

    Create merge_two_sorted.py

---
+++ LeetCode/21/merge_two_sorted.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+
+        head = None
+        curr_node = None
+        if l1.val <= l2.val:
+            head = l1
+            curr_node = l1
+            l1 = l1.next
+        else:
+            head = l2
+            curr_node = l2
+            l2 = l2.next
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr_node.next = l1
+                curr_node = curr_node.next
+                l1 = l1.next
+            else:
+                curr_node.next = l2
+                curr_node = curr_node.next
+                l2 = l2.next
+
+        if l1:
+            curr_node.next = l1
+        if l2:
+            curr_node.next = l2
+
+        return head
