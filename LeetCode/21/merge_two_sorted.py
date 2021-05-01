e6edcc8973370d04b5a73c44bcaf9a19ead72c44
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-05-01 12:14:52 -0700

    Update merge_two_sorted.py

--- LeetCode/21/merge_two_sorted.py
+++ LeetCode/21/merge_two_sorted.py
@@ -1,3 +1,5 @@
+
+# Solution 1: Time complexity O(n + m), Space complexity O(n + m)
 class Solution:
 
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
@@ -32,3 +34,33 @@ class Solution:
             curr_node.next = l2
 
         return head
+
+    
+    
+# Same time and space complexity, O(n + m), but more concise
+class Solution:
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+
+        curr_node = ListNode(val=None, next=None)
+        fake_head = curr_node
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr_node.next = l1
+                l1 = l1.next
+            else:
+                curr_node.next = l2
+                l2 = l2.next
+            curr_node = curr_node.next
+
+        if l1:
+            curr_node.next = l1
+        if l2:
+            curr_node.next = l2
+
+        return fake_head.next
