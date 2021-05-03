1b4e3d6189e103acfb1551d1a4193d1e15e16555
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2021-05-03 16:13:31 -0700

    Update majority_element.py

--- LeetCode/169/majority_element.py
+++ LeetCode/169/majority_element.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = {}
@@ -6,6 +7,5 @@ class Solution:
                 count[num] += 1
             else:
                 count[num] = 1
-
-            if count[num] == len(nums) // 2:
+            if count[num] > len(nums) // 2:
                 return num
