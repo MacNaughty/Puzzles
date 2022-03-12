28d8b8b9ec1b73bf64c31607ce187f725dd8b440
Evan MacNaughton Parrish <evan.cavelaars@outlook.com>
2022-03-12 14:07:34 -0800

    Update two_sum.py

--- LeetCode/1/two_sum.py
+++ LeetCode/1/two_sum.py
@@ -1,19 +1,20 @@
-class Solution:
-  
-  
-# solution 1
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        cache = {1: 1}
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return i, j
+class TestCase(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual([1, 2], Solution.two_sum([3, 2, 4], 6))
 
-        return None
+    def test_2(self):
+        self.assertEqual([0, 1], Solution.two_sum([2, 7, 11, 15], 9))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+class Solution:
       
-# solution 2: two pass hash table
-# Time complexity O(n), space complexity O(n)
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    # solution 1: two pass hash table
+    # Time complexity O(n), space complexity O(n)
+    @classmethod
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
         cache = {}
         for i in range(len(nums)):
             cache[nums[i]] = i
@@ -27,15 +28,20 @@ class Solution:
 
       
       
-# solution 3: one pass hash table 
-# Time complexity O(n), space complexity O(n)
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        cache = {}
-
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in cache:
-                return cache[complement], i
-            cache[nums[i]] = i
+    # solution 2: one pass hash table 
+    # Time complexity O(n), space complexity O(n)
+    @classmethod
+    def two_sum(cls, nums: List[int], target: int) -> List[int]:
+        complement_index_dict = {}
+    
+        for i, num in enumerate(nums):
+            if num in complement_index_dict:
+                return [i, complement_index_dict[num]]
+    
+            complement_index_dict[target - num] = i
 
         return None
+      
+      
+      
+   
